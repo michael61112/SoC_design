2238bdc4f388f77b2047651fdae0c57aff4f5b3d
michael61112 <michael61112@yahoo.com.tw>
2023-12-14 18:40:44 +0800

    Lab4-2: Seperate the wb_decoder from user_proj_example.counter.v

--- Lab4/Lab4_2/lab-exmem_fir/rtl/user/user_proj_example.counter.v
+++ Lab4/Lab4_2/lab-exmem_fir/rtl/user/user_proj_example.counter.v
@@ -35,9 +35,6 @@
  *-------------------------------------------------------------
  */
 
-`define FIR_ADDR 'h3000_0000;
-`define RAM_ADDR 'h3800_0000;
-
 module user_proj_example #(
     parameter BITS = 32,
     parameter DELAYS=10
@@ -56,8 +53,8 @@ module user_proj_example #(
     input [3:0] wbs_sel_i,
     input [31:0] wbs_dat_i,
     input [31:0] wbs_adr_i,
-    output reg wbs_ack_o,
-    output reg [31:0] wbs_dat_o,
+    output wbs_ack_o,
+    output [31:0] wbs_dat_o,
 
     // Logic Analyzer Signals
     input  [127:0] la_data_in,
@@ -72,86 +69,146 @@ module user_proj_example #(
     // IRQ
     output [2:0] irq
 );
-    //=====================================================================
-    //   REG AND WIRE DECLARATION
-    //=====================================================================
     wire clk;
     wire rst;
 
     wire [`MPRJ_IO_PADS-1:0] io_in;
     wire [`MPRJ_IO_PADS-1:0] io_out;
     wire [`MPRJ_IO_PADS-1:0] io_oeb;
-    //========== WB Decoder ==========
-    reg [1:0] sel;
-    // RAM
-    wire ram_wbs_cyc_i;
-    wire ram_wbs_ack_o;
-    wire [31:0] ram_wbs_dat_o;
-    // FIR
-    wire fir_wbs_cyc_i;
-    wire fir_wbs_ack_o;
-    wire [31:0] fir_wbs_dat_o;
 
-    //=====================================================================
-    //   DATA PATH & CONTROL
-    //=====================================================================
-    assign clk = wb_clk_i;
-    assign rst = wb_rst_i;
+    wire [31:0] rdata; 
+    wire [31:0] wdata;
+    reg [BITS-1:0] count;
 
-    always @(*) begin
-        sel[0] = (wbs_adr_i[31:24] == 'h30);    // FIR
-        sel[1] = (wbs_adr_i[31:24] == 'h38);    // RAM
+    wire valid;
+    wire [3:0] wstrb;
+    wire [31:0] la_write;
+    wire decoded;
+
+    reg ready;
+    reg [BITS-17:0] delayed_count;
+
+    // WB MI A
+    assign valid = wbs_cyc_i && wbs_stb_i && decoded; 
+    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
+    assign wbs_dat_o = rdata;
+    assign wdata = wbs_dat_i;
+    assign wbs_ack_o = ready;
+
+    // IO
+    assign io_out = count;
+    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
+
+    // IRQ
+    assign irq = 3'b000;	// Unused
+
+    // LA
+    assign la_data_out = {{(127-BITS){1'b0}}, count};
+    // Assuming LA probes [63:32] are for controlling the count register  
+    assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
+    // Assuming LA probes [65:64] are for controlling the count clk & reset  
+    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
+    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
+
+    assign decoded = wbs_adr_i[31:20] == 12'h380 ? 1'b1 : 1'b0;
+    always @(posedge clk) begin
+        if (rst) begin
+            ready <= 1'b0;
+            delayed_count <= 16'b0;
+        end else begin
+            ready <= 1'b0;
+            if ( valid && !ready ) begin
+                if ( delayed_count == DELAYS ) begin
+                    delayed_count <= 16'b0;
+                    ready <= 1'b1;
+                end else begin
+                    delayed_count <= delayed_count + 1;
+                end
+            end
+        end
     end
-
-    assign ram_wbs_cyc_i = wbs_cyc_i & sel[1];
-    assign fir_wbs_cyc_i = wbs_cyc_i & sel[0];
-
-    always @(*) begin
-        case (sel)
-            'b01: wbs_dat_o = fir_wbs_dat_o;
-            'b10: wbs_dat_o = ram_wbs_dat_o;
-            default: wbs_dat_o = 32'b0;
-        endcase
+    
+    always @(posedge clk) begin
+    	if (rst) begin
+    	    count <= 0;
+    	end else if (count == 0) begin
+    	    if ((wbs_adr_i == 32'h38000000) && valid && (|wstrb == 1'b0)) begin
+    	        count <= count + 1;
+    	    end else begin
+    	        count <= count;
+    	    end
+    	end else begin
+    	    count <= count + 1;
+    	end
     end
-
-    always @(*) begin
-        case (sel)
-            'b01: wbs_ack_o = fir_wbs_ack_o;
-            'b10: wbs_ack_o = ram_wbs_ack_o;
-            default: wbs_ack_o = 1'b0;
-        endcase
-    end
-
-    //========== Exmem-FIR ==========
-    bram_wb user_bram (
-        .wb_clk_i   (clk),
-        .wb_rst_i   (rst),
-        .wbs_stb_i  (wbs_stb_i),
-        .wbs_cyc_i  (ram_wbs_cyc_i),
-        .wbs_we_i   (wbs_we_i),
-        .wbs_sel_i  (wbs_sel_i),
-        .wbs_dat_i  (wbs_dat_i),
-        .wbs_adr_i  (wbs_adr_i),
-        .wbs_ack_o  (ram_wbs_ack_o),
-        .wbs_dat_o  (ram_wbs_dat_o)
+/*
+    counter #(
+        .BITS(BITS)
+    ) counter(
+        .clk(clk),
+        .reset(rst),
+        .ready(wbs_ack_o),
+        .valid(valid),
+        .rdata(rdata),
+        .wdata(wbs_dat_i),
+        .wstrb(wstrb),
+        .la_write(la_write),
+        .la_input(la_data_in[63:32]),
+        .count(count)
     );
+*/
 
-    //========== Verilog-FIR ==========
-    fir_wb user_fir (
-        .wb_clk_i   (clk),
-        .wb_rst_i   (rst),
-        .wbs_stb_i  (wbs_stb_i),
-        .wbs_cyc_i  (fir_wbs_cyc_i),
-        .wbs_we_i   (wbs_we_i),
-        .wbs_sel_i  (wbs_sel_i),
-        .wbs_dat_i  (wbs_dat_i),
-        .wbs_adr_i  (wbs_adr_i),
-        .wbs_ack_o  (fir_wbs_ack_o),
-        .wbs_dat_o  (fir_wbs_dat_o)
+    bram user_bram (
+        .CLK(clk),
+        .WE0(wstrb),
+        .EN0(valid),
+        .Di0(wbs_dat_i),
+        .Do0(rdata),
+        .A0(wbs_adr_i)
     );
 
 endmodule
 
+module counter #(
+    parameter BITS = 32
+)(
+    input clk,
+    input reset,
+    input valid,
+    input [3:0] wstrb,
+    input [BITS-1:0] wdata,
+    input [BITS-1:0] la_write,
+    input [BITS-1:0] la_input,
+    output ready,
+    output [BITS-1:0] rdata,
+    output [BITS-1:0] count
+);
+    reg ready;
+    reg [BITS-1:0] count;
+    reg [BITS-1:0] rdata;
 
+    always @(posedge clk) begin
+        if (reset) begin
+            count <= 0;
+            ready <= 0;
+        end else begin
+            ready <= 1'b0;
+            if (~|la_write) begin
+                count <= count + 1;
+            end
+            if (valid && !ready) begin
+                ready <= 1'b1;
+                rdata <= count;
+                if (wstrb[0]) count[7:0]   <= wdata[7:0];
+                if (wstrb[1]) count[15:8]  <= wdata[15:8];
+                if (wstrb[2]) count[23:16] <= wdata[23:16];
+                if (wstrb[3]) count[31:24] <= wdata[31:24];
+            end else if (|la_write) begin
+                count <= la_write & la_input;
+            end
+        end
+    end
+
+endmodule
 
 `default_nettype wire
