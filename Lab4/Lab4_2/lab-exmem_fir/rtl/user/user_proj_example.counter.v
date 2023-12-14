32429c7e4857dad797a44394196df8a06a881394
michael61112 <michael61112@yahoo.com.tw>
2023-12-14 18:47:10 +0800

    Lab4-2: Remove the example code 'counter' in the user_proj_example.counter.v

--- Lab4/Lab4_2/lab-exmem_fir/rtl/user/user_proj_example.counter.v
+++ Lab4/Lab4_2/lab-exmem_fir/rtl/user/user_proj_example.counter.v
@@ -141,22 +141,6 @@ module user_proj_example #(
     	    count <= count + 1;
     	end
     end
-/*
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
-    );
-*/
 
     bram user_bram (
         .CLK(clk),
@@ -169,46 +153,6 @@ module user_proj_example #(
 
 endmodule
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
-    end
-
-endmodule
 
 `default_nettype wire
